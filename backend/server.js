2d2416aec2acafe3b0003dc55588138ddb9d2337
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-12-27 11:40:56 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -229,11 +229,14 @@ app.get("/api/users", async (req, res) => {
 		const books = [];
 		const users = await User.find();
 		users.forEach((user) => {
-			let member = {
-				name: user.name,
-				books: user.books.toString(),
-			};
-			books.push(member);
+			
+			if (user.books.length > 0) {
+				let member = {
+					name: user.name,
+					books: user.books.toString(),
+				};
+				books.push(member);
+		}
 		});
 		res.status(200).json(books);
 	} catch (err) {
