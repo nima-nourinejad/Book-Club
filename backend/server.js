320e7c97880aeb29d3aefb646046322211ac0f89
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-01-06 14:02:36 +0200

    installed axios on my backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Joi = require("joi");
 const cors = require("cors");
+const axios = require("axios");
 const mongoose = require("mongoose");
 
 const app = express();
